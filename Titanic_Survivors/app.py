2f939e1e8adf917646287a7f725e47adfef6c7f0
Hassan Sheikh <49715207+hsheikh7@users.noreply.github.com>
2023-08-26 20:14:48 +0330

    Update app.py

--- Titanic_Survivors/app.py
+++ Titanic_Survivors/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Titanic_Survivors.utils.py import PrepProcesor, columns 
+# from Titanic_Survivors.utils.py import PrepProcesor, columns 
 
 import numpy as np
 import pandas as pd
@@ -31,3 +31,27 @@ def predict():
 
 trigger = st.button('Predict', on_click=predict)
 
+
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer 
+import re
+
+numeric_columns = ['squareMeters', 'numberOfRooms', 'floors', 'numPrevOwners']
+categorical_columns = ['cityCode', 'made', 'basement', 'attic', 'garage']
+
+class PrepProcesor(BaseEstimator, TransformerMixin): 
+    def fit(self, X, y=None): 
+        self.ageImputer = SimpleImputer()
+        self.ageImputer.fit(X[['Age']])        
+        return self 
+        
+    def transform(self, X, y=None):
+        X['Age'] = self.ageImputer.transform(X[['Age']])
+        X['CabinClass'] = X['Cabin'].fillna('M').apply(lambda x: str(x).replace(" ", "")).apply(lambda x: re.sub(r'[^a-zA-Z]', '', x))
+        X['CabinNumber'] = X['Cabin'].fillna('M').apply(lambda x: str(x).replace(" ", "")).apply(lambda x: re.sub(r'[^0-9]', '', x)).replace('', 0) 
+        X['Embarked'] = X['Embarked'].fillna('M')
+        X = X.drop(['PassengerId', 'Name', 'Ticket','Cabin'], axis=1)
+        return X
+
+columns = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch','Ticket', 'Fare', 'Cabin', 'Embarked']
