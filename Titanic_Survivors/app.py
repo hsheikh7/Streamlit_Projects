6d71dfd5d7e64f5cb7a03dd03b7ef1d4ef12fcee
Hassan Sheikh <49715207+hsheikh7@users.noreply.github.com>
2023-08-26 20:11:13 +0330

    Update app.py

--- Titanic_Survivors/app.py
+++ Titanic_Survivors/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Titanic_Survivors.utils import PrepProcesor, columns 
+from Titanic_Survivors.utils.py import PrepProcesor, columns 
 
 import numpy as np
 import pandas as pd
